ef5dba54ecb08d6dcb99e970c6a9602fd4021706
vingensys <vingensys@gmail.com>
2017-09-27 19:01:54 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-#! /home/vinod/neuralnetw/python_rest/restapi/bin/python3
+##! provide the path to your python3 interpretter
 # Run a test server.
 from MenuMaker import app
 app.run(host='127.0.0.1', port=8080, debug=True)
